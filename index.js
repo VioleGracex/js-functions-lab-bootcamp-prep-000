001bb6cd6ea4a9e617a106ab52f223d8e6546a3c
highdevilxx@hotmail.com Khaled <highdevilxx@hotmail.com>
2018-08-04 12:12:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function happyHolidaysTo(name)
 {
   return  "Happy ${holiday}, ${name}!";
 }*/
-function holidayCountdown(days,holiday)
+function holidayCountdown(holiday,days)
 {
-  return "It's ${days}! days until ${holiday}!";
+  return `It's ${days} days until ${holiday}!`;
 }
